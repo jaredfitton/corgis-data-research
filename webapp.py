3c67be2eb5f3a0f5faac68fccd7f7c5f53987a28
jaredfitton <jaredfitton@gmail.com>
2017-12-01 10:17:57 -0800

    Added python for most_delays page

--- webapp.py
+++ webapp.py
@@ -1,4 +1,6 @@
-from flask import Flask, url_for, render_template, request
+from flask import Flask, url_for, render_template, request, Markup
+import os
+import json
 
 app = Flask(__name__)
 
@@ -8,4 +10,19 @@ def render_main():
 
 @app.route("/most_delays")
 def render_meter():
-    return render_template('most_delays.html')
+    with open('static/airlines.json') as airlines_data:
+        airlines = json.load(airlines_data)
+    return render_template('most_delays.html', response_airport_list = get_airport_list(airlines))
+
+def get_airport_list(airlines):
+    airports = []
+    airport_list = ""
+    for a in airlines:
+        if a["airport"]["name"] not in airports:
+            airports.append(a["airport"]["name"])
+            airport_list += Markup("<option value=\"" + a["airport"]["name"] + "\">" + a["airport"]["name"] + "</option>")
+    return airport_list
+
+
+if __name__=="__main__":
+    app.run(debug=True, port=54321)
