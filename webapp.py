a20c9b025d2945aab13d9826eceb383ca0e1be74
jaredfitton <jaredfitton@gmail.com>
2017-12-04 10:00:05 -0800

    Started adding pie chart

--- webapp.py
+++ webapp.py
@@ -12,6 +12,9 @@ def render_main():
 def render_meter():
     with open('static/airlines.json') as airlines_data:
         airlines = json.load(airlines_data)
+    if 'Airport' in request.args:
+        selected_airport = request.args['Airport']
+        return render_template('most_delays.html', response_airport_list = get_airport_list(airlines), response_airport = selected_airport, response_delays = get_airport_delays(airlines, selected_airport))
     return render_template('most_delays.html', response_airport_list = get_airport_list(airlines))
 
 def get_airport_list(airlines):
@@ -22,6 +25,13 @@ def get_airport_list(airlines):
             airports.append(a["airport"]["name"])
             airport_list += Markup("<option value=\"" + a["airport"]["name"] + "\">" + a["airport"]["name"] + "</option>")
     return airport_list
+
+def get_airport_delays(airlines, airport):
+    total_delays = 0
+    for a in airlines:
+        if a["airport"]["name"] == airport:
+            total_delays += 5
+    return total_delays
 
 
 if __name__=="__main__":
