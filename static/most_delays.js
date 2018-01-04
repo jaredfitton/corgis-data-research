adcb71578ca2a6f33e1525ca769f33e264e4c745
jaredfitton <jaredfitton@gmail.com>
2018-01-04 09:18:03 -0800

    Added Pie Chart

--- static/most_delays.js
+++ static/most_delays.js
@@ -1,31 +1,3 @@
 $(document).ready(function(){
-    // $("#chartContainer").CanvasJSChart({
-	// 	title: {
-	// 		text: "Worldwide Smartphone sales by brand - 2012",
-	// 		fontSize: 24
-	// 	},
-	// 	axisY: {
-	// 		title: "Products in %"
-	// 	},
-	// 	legend :{
-	// 		verticalAlign: "center",
-	// 		horizontalAlign: "right"
-	// 	},
-	// 	data: [
-	// 	{
-	// 		type: "pie",
-	// 		showInLegend: true,
-	// 		toolTipContent: "{label} <br/> {y} %",
-	// 		indexLabel: "{y} %",
-	// 		dataPoints: [
-	// 			{ label: "Samsung",  y: 30.3, legendText: "Samsung"},
-	// 			{ label: "Apple",    y: 19.1, legendText: "Apple"  },
-	// 			{ label: "Huawei",   y: 4.0,  legendText: "Huawei" },
-	// 			{ label: "LG",       y: 3.8,  legendText: "LG Electronics"},
-	// 			{ label: "Lenovo",   y: 3.2,  legendText: "Lenovo" },
-	// 			{ label: "Others",   y: 39.6, legendText: "Others" }
-	// 		]
-	// 	}
-	// 	]
-	// });
+    
 });
