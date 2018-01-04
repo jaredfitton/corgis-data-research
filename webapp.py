4dc8f8ec2530f8ee65995f80ad7040e44a0938fe
jaredfitton <jaredfitton@gmail.com>
2018-01-04 10:27:16 -0800

    Added year select

--- webapp.py
+++ webapp.py
@@ -12,10 +12,11 @@ def render_main():
 def render_meter():
     with open('static/airlines.json') as airlines_data:
         airlines = json.load(airlines_data)
-    if 'Airport' in request.args:
+    if 'Airport' in request.args and 'Year' in request.args:
         selected_airport = request.args['Airport']
-        return render_template('most_delays.html', response_airport_list = get_airport_list(airlines), response_airport = selected_airport, response_delays = get_airport_delays(airlines, selected_airport))
-    return render_template('most_delays.html', response_airport_list = get_airport_list(airlines))
+        selected_year = request.args['Year']
+        return render_template('most_delays.html', response_airport_list = get_airport_list(airlines), response_years = get_years(airlines), response_airport = selected_airport, response_year = selected_year, response_delays = get_airport_delays(airlines, selected_airport, selected_year))
+    return render_template('most_delays.html', response_airport_list = get_airport_list(airlines), response_years = get_years(airlines))
 
 def get_airport_list(airlines):
     airports = []
@@ -26,13 +27,18 @@ def get_airport_list(airlines):
             airport_list += Markup("<option value=\"" + a["airport"]["name"] + "\">" + a["airport"]["name"] + "</option>")
     return airport_list
 
-def get_airport_delays(airlines, airport):
+def get_airport_delays(airlines, airport, year):
     total_delays = 0
     for a in airlines:
-        if a["airport"]["name"] == airport:
-            total_delays += 5
+        if a["airport"]["name"] == airport and a["time"]["year"] == int(year):
+            total_delays += a["statistics"]["flights"]["delayed"]
     return total_delays
 
+def get_years(airlines):
+    years = ""
+    for a in range(2003, 2016):
+        years += Markup("<option value=\"" + str(a) + "\">" + str(a) + "</option>")
+    return years
 
 if __name__=="__main__":
     app.run(debug=True, port=54321)
